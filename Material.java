f29e9abb59d6e9feb3293abaa47144ee0f575f06
abm0028 <78190672+abm0028@users.noreply.github.com>
2021-04-18 23:26:19 -0500

    Update Material.java

--- Material.java
+++ Material.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.cs321_thealchemist;
-import java.util.*;
-/**
- *
- * @author Alec
- */
 public class Material {
     /*
     private:
