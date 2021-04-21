c81a91c31b404a3f303cf9f8c06b7b3d35459dbb
GhettoBuddha <78190734+GhettoBuddha@users.noreply.github.com>
2021-04-21 14:24:27 -0500

    Update Droplet.java

--- Droplet.java
+++ Droplet.java
@@ -1,4 +1,8 @@
 public class Droplet {
+    
+    /**
+    * The update_state method will compare the temperature to the melting points and boiling points to determine what state the chemical should be in.
+    */
     public void update_state() {    //Could probably combine w/ update_density()
         if (temperature < chem_type.get_melt_point())
             current_state = State.solid;
@@ -7,6 +11,10 @@ public class Droplet {
         if (temperature >= chem_type.get_boil_point())
             current_state = State.gas;
     }
+    
+    /**
+    * The update_density method will check the chemical's state and give it the corresponding density for it.
+    */
     public void update_density() {
         if (current_state == State.solid)
             density = chem_type.get_sol_density();
@@ -15,10 +23,18 @@ public class Droplet {
         if (current_state == State.gas)
             density = chem_type.get_gas_density();
     }
+    
+    /**
+    * The update_temp will
+    */
     public void update_temp() {     //^^^
         temperature = temperature + dTemp;
     }
     //public Droplet clone() {}
+    /**
+    * The copy_into will take the inputted droplet and set its variables equal to the one calling the function
+    * @param droplet, the droplet that will have everything copied into
+    */
     public void copy_into(Droplet droplet) {
         droplet.set_chem_type(chem_type);
         droplet.set_temperature(temperature);
@@ -30,15 +46,55 @@ public class Droplet {
         droplet.set_num_compressed(num_compressed);
     }
 
+    //getters
+    /**
+    * The get_chem_type method will return what type of chemical the droplet is
+    * @return chem_type, the type of chemical
+    */ 
     public Chemical get_chem_type() { return chem_type; }
+    
+    /**
+    * The get_temperature method will return the temperature of the droplet
+    * @return temperature, the current temperature of the droplet
+    */
     public double get_temperature() { return temperature; }
+    
+    /**
+    * The get_density method will return the density of the droplet
+    * @return density, the current density of the droplet
+    */
     public double get_density() { return density; }
+    
+    /**
+    * The get_current_state method will return the state(solid, liquid, gas) of the droplet
+    * @return current_state, the state the droplet is in
+    */
     public State get_current_state() { return current_state; }
+    
+    /**
+    * The get_dTemp will return 
+    * @return dTemp
+    */
     public double get_dTemp() { return dTemp; }
+    
+    /**
+    * The get_has_moved method will return true or false to whether the droplet has moved
+    * @return has_moved, true/false
+    */
     public boolean get_has_moved() { return has_moved; }
+    
+    /**
+    * The get_has_reacted method will return true/false to whether the droplet has reacted
+    * @return has_reacted, true/false
+    */
     public boolean get_has_reacted() { return has_reacted; }
+    
+    /**
+    * The get_num_compressed method will 
+    */
     public int get_num_compressed() { return num_compressed; }
 
+    //setters
     public void set_chem_type(Chemical new_value) { chem_type = new_value; }
     public void set_temperature(double new_value) { temperature = new_value; }
     public void set_density(double new_value) { density = new_value; }
