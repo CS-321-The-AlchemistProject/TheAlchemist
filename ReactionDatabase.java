20eef777a3696635664d188d27bff640b48e643a
Sakardhakal01 <58885231+Sakardhakal01@users.noreply.github.com>
2021-04-23 11:17:26 -0500

    Update ReactionDatabase.java

--- ReactionDatabase.java
+++ ReactionDatabase.java
@@ -1,10 +1,33 @@
-public class ReactionDatabase {
-    public void sort() {}
-    public int search(String reaction_key) {}   //Returns index if target reaction
-    public Reaction get_reaction(int index) { return reaction_list[index]; }
-    public void set_reaction(int index, Reaction new_value) { reaction_list[index] = new_value; }
-    public void add_reaction(Reaction new_value) {}
-    public boolean delete_reaction(String chem_key) {}
+import java.util.HashMap;
+import java.util.ArrayList;
 
-    private Reaction[] reaction_list;
+public class ReactionDatabase {
+	public boolean find(String reaction_key) { 
+		return reactions.containsKey(reaction_key); 
+	}
+
+	public ArrayList<Reaction> get_reaction(String reaction_key) { 
+		return reactions.get(reaction_key); 
+	}
+
+	public void set_reaction(String reaction_key, ArrayList<Reaction> new_value) { 
+		if(delete_reaction(reaction_key)) 
+			reactions.put(reaction_key, new_value);	
+	}
+
+	public void add_reaction(String reaction_key, Reaction new_value) { 
+		if(reactions.get(reaction_key) == null) {		
+			reactions.put(reaction_key, new ArrayList<Reaction>());
+		}
+		reactions.get(reaction_key).add(new_value);
+	}
+
+	public boolean delete_reaction(String reaction_key) {
+		if(reactions.get(reaction_key) == null) 		
+			return false;
+		reactions.remove(reaction_key);
+		return true;
+	}
+
+	private HashMap<String, ArrayList<Reaction>> reactions;
 }
