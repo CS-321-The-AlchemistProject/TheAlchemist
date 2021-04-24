644012915aa9bd51d3a3afe5a664709fc72ffdbf
Sakardhakal01 <58885231+Sakardhakal01@users.noreply.github.com>
2021-04-23 19:10:35 -0500

    Update ChemicalDatabase.java

--- ChemicalDatabase.java
+++ ChemicalDatabase.java
@@ -1,40 +1,88 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChemicalDatabase {
 
-	/**
-	* The ChemicalDatabase constructor will create a new array list of chemical type
-	*/
 	public ChemicalDatabase() {
 		chemical_list = new ArrayList<Chemical>();
+		formulaMap = new HashMap<String, String>();
 	}
 
-	/**
-	* The sort method will call the quickSort method with inputted parameters
-	*/
+	public void initialize_db() {
+		try{
+			File f = new File("ChemicalList.txt");
+			Scanner s = new Scanner(f);
+			while(s.hasNextLine()) {
+				String line = s.nextLine();
+				String[] tempList = line.split(" ");	
+				Chemical entry = new Chemical();
+				int i = 0;
+				entry.set_formula(tempList[i++]);
+				String name = tempList[i++]; 
+				while(!Character.isDigit(tempList[i].charAt(0)))
+					name += " " + tempList[i++];
+				entry.set_name(name);
+				entry.set_CAS(tempList[i++]);
+				entry.set_melt_point(Double.parseDouble(tempList[i++]));
+				entry.set_boil_point(Double.parseDouble(tempList[i++]));
+
+				entry.set_sp_heat_solid(Double.parseDouble(tempList[i++]));
+				entry.set_thermal_cond_solid(Double.parseDouble(tempList[i++]));
+				entry.set_thermal_diff_solid(Double.parseDouble(tempList[i++]));
+				entry.set_sol_density(Double.parseDouble(tempList[i++]));
+			
+				entry.set_sp_heat_liquid(Double.parseDouble(tempList[i++]));
+				entry.set_thermal_cond_liquid(Double.parseDouble(tempList[i++]));
+				entry.set_thermal_diff_liquid(Double.parseDouble(tempList[i++]));
+				entry.set_liq_density(Double.parseDouble(tempList[i++]));
+ 
+				entry.set_sp_heat_gas(Double.parseDouble(tempList[i++]));
+				entry.set_thermal_cond_gas(Double.parseDouble(tempList[i++]));
+				entry.set_thermal_diff_gas(Double.parseDouble(tempList[i++]));
+				entry.set_gas_density(Double.parseDouble(tempList[i++])); 
+	
+				entry.set_color(tempList[i++]);
+				entry.set_chem_key(tempList[0]);
+				//entry.set_is_insulated (tempList.get()) no info in data				
+
+				if(formulaMap.get(tempList[0]) == null)
+					formulaMap.put(tempList[0], name);			
+				chemical_list.add(entry);
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println("not found");
+		}
+		sort();
+	}
+
+/*
+formula common_name CAS_number melting_point boiling_point specific_heat_solid thermal_conductivity_solid thermal_diffusivity_solid 
+ density_solid specific_heat_liquid thermal_conductivity_liquid thermal_diffusivity_liquid 
+ density_liquid specific_heat_gas thermal_conductivity_gas thermal_diffusivity_gas 
+ density_gas color_code
+*/
 	public void sort() {
 		quickSort(0, chemical_list.size()-1);
 	}
 
-	/**
-	* The quickSort method will use the quick sort algorithm to order the chemical list
-	* @param low, the lowest index
-	* @param high, the highest index
-	*/
 	private void quickSort(int low, int high) {
 		int i = low, j = high;
 		Chemical pivot = chemical_list.get(low + (high - low) / 2);
 		while (i <= j) {
 	    		while ((chemical_list.get(i).get_name()).compareToIgnoreCase(pivot.get_name()) < 0) {
-				    i++;
+				i++;
 	    		}
 	    		while ((chemical_list.get(j).get_name()).compareToIgnoreCase(pivot.get_name()) > 0) {
-				    j--;
+				j--;
 	    		}
 	    		if (i <= j) {
-				    exchange(i, j);
-				    i++;
-				    j--;
+				exchange(i, j);
+				i++;
+				j--;
 	    		}
 		}
 		if (low < j) {
@@ -45,21 +93,12 @@ public class ChemicalDatabase {
 		}	
 	}
 
-	/**
-	* The exchange method will swap the indexes of two chemicals
-	* @param i, the index of the first chemical
-	* @param j, the index of the second chemical
-	*/
 	private void exchange(int i, int j) {
         	Chemical temp = chemical_list.get(i);
         	chemical_list.set(i, chemical_list.get(j));
         	chemical_list.set(j, temp);
-    }
+    	}
 
-	/**
-	* The search method will use the inputted string and search through the list for the key
-	* @param chem_key, the access key for the chemical
-	*/
 	public int search(String chem_key) {
 		int low = 0;
 		int high = chemical_list.size() - 1;
@@ -68,51 +107,41 @@ public class ChemicalDatabase {
 		    mid = (low + high) / 2;
 
 		    if (chemical_list.get(mid).get_name().compareTo(chem_key) < 0) {
-			    low = mid + 1;
-		    } 
-            else if (chemical_list.get(mid).get_name().compareTo(chem_key) > 0) {
-			    high = mid - 1;
-		    } 
-            else {
-			    return mid;
+			low = mid + 1;
+		    } else if (chemical_list.get(mid).get_name().compareTo(chem_key) > 0) {
+			high = mid - 1;
+		    } else {
+			return mid;
 		    }
 		}
 		return -1;
 	}   //Returns index of target chemical else -1
+	
+	public void print_formulaMap() {
+		for (Map.Entry<String, String> entry : formulaMap.entrySet()) {
+		    	String key = entry.getKey();
+		    	String val = entry.getValue();
+		    	System.out.println(key + " : " + val);
+	        }
+	}
 
-	/**
-	* The print_list method will print the whole list of chemicals out
-	*/
+	public String convert_formula_to_name(String chem_formula) {
+		return formulaMap.get(chem_formula);	
+	}
+
 	public void print_list() {
 		for(int i = 0; i < chemical_list.size(); i++)
 			System.out.println(chemical_list.get(i).get_name());	
 	}
 
-	/**
-	* The get_chemical will return the chemical at a certain index
-	* @param index, the position of the chemical in the list
-	*/
 	public Chemical get_chemical(int index) { return chemical_list.get(index); }
 
-	/**
-	* The set_chemical will change the value of the chemical at a certain index
-	* @param index, the position of the chemical in the list
-	* @param new_value, the value that the chemical will change to
-	*/
 	public void set_chemical(int index, Chemical new_value) { chemical_list.set(index, new_value); }
 
-	/**
-	* The add_chemical method will add a new chemical to the list
-	* @param new_value, the new chemical being added
-	*/
 	public void add_chemical(Chemical new_value) {
 		chemical_list.add(new_value);	
 	}
 
-	/**
-	* The delete_chemical will remove a chemical from the list by searching for it using the chemical key
-	* @param chem_key, the key of the chemical to be removed
-	*/
 	public boolean delete_chemical(String chem_key) {
 		int k = search(chem_key);			
 		chemical_list.remove(k);
@@ -120,4 +149,5 @@ public class ChemicalDatabase {
 	}
 
 	private ArrayList<Chemical> chemical_list;
+	private HashMap<String, String> formulaMap;
 }
