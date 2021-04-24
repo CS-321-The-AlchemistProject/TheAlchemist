4218852d5c7ac824fb4b1336530c4b47602d2b12
GhettoBuddha <78190734+GhettoBuddha@users.noreply.github.com>
2021-04-23 22:31:38 -0500

    Update Chemical.java

--- src/AlchMain/Chemical.java
+++ src/AlchMain/Chemical.java
@@ -106,31 +106,110 @@ public class Chemical {
     */
     public boolean get_is_insulated() { return is_insulated; }
 
+    /**
+    * The set_name method will change the chemical's name to the inputted value.
+    * @param new_value, the new name for the chemical
+    */
     public void set_name(String new_value) { chem_name = new_value; }
+    /**
+    * The set_formula method will change the chemical's formula to the inputted value.
+    * @param new_value, the new formula for the chemical
+    */
     public void set_formula(String new_value) { chem_formula = new_value; }
 
+    /**
+    * The set_thermal_diff_solid method will change the solid's thermal diffusivity to the inputted value.
+    * @param new_value, the new value for the thermal diffusivity
+    */
     public void set_thermal_diff_solid(double new_value) { thermal_diffusivity_solid = new_value; }
+    /**
+    * The set_thermal_diff_liquid method will change the liquid's thermal diffusivity to the inputted value.
+    * @param new_value, the new value for the thermal diffusivity
+    */
     public void set_thermal_diff_liquid(double new_value) { thermal_diffusivity_liquid = new_value; }
+    /**
+    * The set_thermal_diff_gas method will change the value of the thermal diffusivity for the gas to the inputted value.
+    * @param new_value, the new value for the thermal diffusivity
+    */
     public void set_thermal_diff_gas(double new_value) { thermal_diffusivity_gas = new_value; }
 
+    /**
+    * The set_thermal_cond_solid method will change the solid's thermal conductivity to the inputted value.
+    * @param new_value, the new value for the thermal conductivity
+    */
     public void set_thermal_cond_solid(double new_value) { thermal_conductivity_solid = new_value; }
+    /**
+    * The set_thermal_cond_liquid method will change the liquid's thermal conductivity to the inputted value.
+    * @param new_value, the new value for the thermal conductivity
+    */
     public void set_thermal_cond_liquid(double new_value) { thermal_conductivity_liquid = new_value; }
+    /**
+    * The set_thermal_cond_gas method will change the value of thermal conductivity for the gas to the inputted value.
+    * @param new_value, the new value for the thermal conductivity
+    */
     public void set_thermal_cond_gas(double new_value) { thermal_conductivity_gas = new_value; }
 
+    /**
+    * The set_sp_heat_solid method will change the solid's specific heat to the inputted value.
+    * @param new_value, the new value for the specific heat
+    */
     public void set_sp_heat_solid(double new_value) { specific_heat_solid = new_value; }
+    /**
+    * The set_sp_heat_liquid method will change the liquid's specific heat to the inputted value.
+    * @param new_value, the new value for the specific heat
+    */
     public void set_sp_heat_liquid(double new_value) { specific_heat_liquid = new_value; }
+    /**
+    * The set_sp_heat_gas method will change the specific heat of the gas to the inputted value.
+    * @param new_value, the new value for the specific heat
+    */
     public void set_sp_heat_gas(double new_value) { specific_heat_gas = new_value; }
-
+    /**
+    * The set_melt_point method will change the value of the melting point of the chemical to the inputted value.
+    * @param new_value, the new value for the melting point
+    */
     public void set_melt_point(double new_value) { melt_point = new_value; }
+    /**
+    * The set_boil_point method will change the value of the boiling point of the chemical to the inputted value.
+    * @param new_value, the new value for the boiling point
+    */
     public void set_boil_point(double new_value) { boil_point = new_value; }
 
+    /**
+    * The set_gas_density method will change the value of the density of the gas to the inputted value.
+    * @param new_value, the new value for the density
+    */
     public void set_gas_density(double new_value) { gas_density = new_value; }
+    /**
+    * The set_liq_density method will change the value of the density of the liquid to the inputted value.
+    * @param new_value, the new value for the density
+    */
     public void set_liq_density(double new_value) { liq_density = new_value; }
+    /**
+    * The set_sol_density method will change the value of the density of the solid to the inputted value.
+    * @param new_value, the new value for the density
+    */
     public void set_sol_density(double new_value) { sol_density = new_value; }
 
+    /**
+    * The set_color method will change the color of the chemical to the inputted color.
+    * @param new_value, the new color
+    */
     public void set_color(String new_value) { color = new_value; }
+    /**
+    * The set_chem_key method will change the key of the chemical to the inputted value.
+    * @param new_value, the new key of the chemical
+    */
     public void set_chem_key(String new_value) { chem_key = new_value; }
+    /**
+    * The set_is_insulated will change the value of the variable is_insulated, which determines if the block is a barrier or not.
+    * @param new_value, true/false
+    */
     public void set_is_insulated (boolean new_value) { is_insulated = new_value; }
+    /**
+    * The set_CAS method will change the CAS number of the chemical to the inputted value.
+    * @param new_value, the new value for the CAS number
+    */
     public void set_CAS (String new_value) { CAS_num = new_value; }
 
 
